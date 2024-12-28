6770f1391ef072a64a517ee93c98ea8defc4bbfc
Vigneshlearneveryday <nauticalvigneshmenon@gmail.com>
2024-12-28 19:28:26 +0530

    Practice

---
+++ Number DataType/script.js
@@ -0,0 +1,16 @@
+// Methods 
+
+// 1: tofixed()
+let amount = 49.9753;
+let result = amount.toFixed(1);
+// console.log(result);
+
+// 2: parseFloat() and parseInt()
+
+let qty = "50";
+let convertedNumber = parseFloat(qty);
+console.log(convertedNumber);
+console.log(qty);
+console.log(typeof convertedNumber);
+console.log(typeof qty);
+
