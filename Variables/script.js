73e617d97d8cf72baa5518635e0e6dcdde971da8
Vigneshlearneveryday <nauticalvigneshmenon@gmail.com>
2024-12-28 14:13:37 +0530

    JS Variable

---
+++ Variables/script.js
@@ -0,0 +1,29 @@
+//!----- Using `var`----
+
+//Task: Storing a person's first name
+
+// Variable Declaration and Initialization
+
+// Declaration and Initialization in one line
+
+let firstName = 'Vignesh';
+// console.log(firstName);
+
+// Declaration first and then Initialization
+var country,
+country = 'India';
+// console.log(country);
+
+// multiple declaration and initialization
+let a, b, c;
+a = 1;
+b = 2;
+c = 3;
+// console.log(a, b, c);
+
+//Multiple Declaration in one line
+
+let x = "Vignesh D" , y = "JavaScript" , z = "MERN stack";
+console.log(x, y, z);
+let m = 9, n = 10, o = 11;
+// console.log(m, n, o);
